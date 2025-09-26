e8e15e8f92d750ccaf5aa6a45b5527534211efae
Guillaume Allais <guillaume.allais@ens-lyon.org>
2025-09-26 10:37:36 +0100

    [ lec2 ] skeleton

---
+++ lecture-notes/Week01Live2.hs
@@ -0,0 +1,69 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+module Week01Lecture2 where
+
+import Data.List
+
+
+------------------------------------------------------------------------------
+-- A small Markup data structure
+-- Related work: This is the type of generic representation used in e.g. pandoc
+-- https://pandoc.org/
+
+
+-- DEFINE data Markup
+-- text, bold, italic, concat
+
+
+-- DEFINE an example: hello world (with some random
+
+
+-- DISCUSS syntax vs. semantics based on example
+
+
+
+------------------------------------------------------------------------------
+-- A small Markup language
+
+-- DISCUSS Domain Specific Languages
+-- Nouns: datatypes / ground values
+-- Verbs: functions
+
+
+-- DEFINE catMarkup
+-- catMarkup :: [Markup] -> Markup
+
+
+-- DEFINE
+-- catMarkupSpaced [Text "hello", Text "world"]
+--   Concat (Text "hello") (Concat (Text " ") (Text "world"))
+
+-- catMarkupSpaced :: [Markup] -> Markup
+
+-- REFACTOR as punctuate
+
+
+-- DEFINE list :: [Markup] -> Markup
+-- DEFINE set  :: [Markup] -> Markup
+
+-- REFACTOR list, set
+
+
+------------------------------------------------------------------------------
+-- A small Markup semantics
+
+-- DEFINE HTML
+
+
+-- DEFINE bold
+-- DEFINE italic
+
+
+-- DISCUSS Yet Another DSL!
+
+
+-- DEFINE markupToHTML :: Markup -> [HTML]
+
+
+
+
+-- DISCUSS (and DEFINE?) escapeHTML
