0e9ddf312faef0173e2800a580f2ccacc787cd59
Guillaume Allais <guillaume.allais@ens-lyon.org>
2025-10-20 10:06:17 +0100

    [ lec ] notes from Friday

--- lecture-notes/Week04Live.hs
+++ lecture-notes/Week04Live.hs
@@ -163,14 +163,39 @@ nommoctracing = nommoc "acc" (\ str x -> "(" ++ str ++ ") `upd` " ++ show x)
 
 -- DEFINE data Bank
 
+data Bank = Account Integer Integer
+ deriving Show
+
+
+
 -- DEFINE Transaction
+data Transaction = CreditA Integer
+                  | DebitA Integer
+                  | CreditB Integer
+                  | DebitB Integer
+                  | TransferAtoB Integer
+                  | DoNothing
+  deriving Show
+
+step :: Bank -> Transaction -> Bank
+step (Account a b) (CreditA x) = Account (x+a) b
+step (Account a b) (DebitA x)  = Account (a - x) b
+step (Account a b) (CreditB x) = Account a (b + x)
+step (Account a b) (DebitB x)  = Account a (b - x)
+step (Account a b) (TransferAtoB x) = Account (a - x) (b + x)
+step (Account a b) DoNothing   = Account a b
 
 
 -- DEFINE the semantics of transaction as a Bank-transformer
 
 -- DEFINE an example of a list of transactions
-
+dayTransaction :: [Transaction]
+dayTransaction = [DebitB 40, DoNothing, TransferAtoB 40, DebitB 40]
 -- DEFINE an initial bank
+
+
+initialBalance :: Bank
+initialBalance = Account 0 0
 
 
 
@@ -205,12 +230,34 @@ nommoctracing = nommoc "acc" (\ str x -> "(" ++ str ++ ") `upd` " ++ show x)
 
 -- DEFINE (Formula a)
 
+data Formula a
+  = Atom a
+  | Not (Formula a)
+  | And (Formula a) (Formula a)
+  | Or  (Formula a) (Formula a)
+  deriving (Show)
+
 -- DEFINE an example formula
 myFormula :: Formula String
 myFormula = Not (And (Atom "X") (Atom "Y"))
 
 
 -- DEFINE foldrFormula (same type as for lists) for formulas
+
+foldrFormula :: (a -> b -> b) -> b -> Formula a -> b
+foldrFormula c n f = case f of
+  Atom a -> c a n
+  Not f1 -> foldrFormula c n f1
+  And f1 f2 -> let n2 = foldrFormula c n f2 in
+               foldrFormula c n2 f1
+  Or f1 f2 -> let n2 = foldrFormula c n f2 in
+               foldrFormula c n2 f1
+
+mapFormula :: (a -> b) -> Formula a -> Formula b
+mapFormula = undefined
+
+allAtoms :: Formula a -> [a]
+allAtoms = foldrFormula (:) []
 
 
 -- DISCUSS lack of control
@@ -220,3 +267,44 @@ myFormula = Not (And (Atom "X") (Atom "Y"))
 -- DEFINE example (e.g. 'simplify' pushing negations inwards)
 
 -- DISCUSS a property linking foldr, foldrFormula, and foldFormula
+
+data FormulaShape a r
+  = AtomShape a
+  | NotShape r
+  | AndShape r r
+  | OrShape r r
+  deriving (Show)
+
+data RoseTree a = MkRose a ([RoseTree a])
+
+data FullFormula a = MkFull (FormulaShape a (FullFormula a))
+
+mapFormulaShape :: (r -> s) -> FormulaShape a r -> FormulaShape a s
+mapFormulaShape f shp = case shp of
+  AtomShape a -> AtomShape a
+  NotShape shp1 -> NotShape (f shp1)
+  AndShape shp1 shp2 -> AndShape (f shp1) (f shp2)
+  OrShape shp1 shp2 -> OrShape (f shp1) (f shp2)
+
+fold :: (FormulaShape a b -> b) -> (FullFormula a -> b)
+fold alg (MkFull shp) = alg (mapFormulaShape (fold alg) shp)
+
+
+nOt :: FullFormula a -> FullFormula a
+nOt = MkFull . NotShape
+
+aNd :: FullFormula a -> FullFormula a -> FullFormula a
+aNd f g = MkFull (AndShape f g)
+
+oRr :: FullFormula a -> FullFormula a -> FullFormula a
+oRr f g = MkFull (OrShape f g)
+
+simplify :: FullFormula a -> Bool -> FullFormula a
+simplify = fold alg where
+
+  alg :: FormulaShape a (Bool -> FullFormula a)
+      ->                (Bool -> FullFormula a)
+  alg (AtomShape a) b = (if b then nOt else id) (MkFull (AtomShape a))
+  alg (NotShape bf) b = bf (not b)
+  alg (AndShape bf1 bf2) b = (if b then oRr else aNd) (bf1 b) (bf2 b)
+  alg (OrShape bf1 bf2) b = (if b then aNd else oRr) (bf1 b) (bf2 b)
