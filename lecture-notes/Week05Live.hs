fde30a5ff05a40c0d0c88442d4adedb1459eba6a
Guillaume Allais <guillaume.allais@ens-lyon.org>
2025-10-27 14:58:45 +0000

    Friday's live code

--- lecture-notes/Week05Live.hs
+++ lecture-notes/Week05Live.hs
@@ -5,6 +5,8 @@ module Week05Live where
 import Prelude hiding (Semigroup (..), Monoid (..), Foldable (..), Functor (..))
 import Data.Char
 
+import Test.QuickCheck
+
 -- Next week: 28th October: Class Test (1-3pm LT1201, 50%).
 
 ------------------------------------------------------------------------
@@ -144,31 +146,37 @@ class Semigroup m where
 instance Semigroup [a] where
   (<>) = (++)
 
-newtype Tsil a = MkTsil { getTsil :: [a] } deriving Show
+newtype Tsil a = MkTsil { getTsil :: [a] } deriving (Show, Eq, Arbitrary)
 
 instance Semigroup (Tsil a) where
-  MkTsil xs <> MkTsil ys = ys ++ xs
+  MkTsil xs <> MkTsil ys = MkTsil (ys ++ xs)
 
-newtype Empty a = MkEmpty { getEmpty :: [a] } deriving Show
+newtype Empty a = MkEmpty { getEmpty :: [a] } deriving (Show, Eq, Arbitrary)
 
 instance Semigroup (Empty a) where
   _ <> _ = MkEmpty []
 
-newtype Sum = MkSum {getSum :: Int} deriving Show
+newtype Sum = MkSum {getSum :: Int} deriving (Show, Eq, Arbitrary)
 
 instance Semigroup Sum where
   MkSum n <> MkSum m = MkSum (n + m)
 
-newtype Prod = MkProd {getProd :: Int} deriving Show
+newtype Prod = MkProd {getProd :: Int} deriving (Show, Eq, Arbitrary)
 
 instance Semigroup Prod where
   MkProd n <> MkProd m = MkProd (n*m)
 
-newtype Max = MkMax {getMax :: Int} deriving Show
+newtype Max = MkMax {getMax :: Int} deriving (Show, Eq, Arbitrary)
 
 instance Semigroup Max where
   MkMax n <> MkMax m = MkMax (max n m)
 
+{-
+newtype Sub = MkSub { getSub :: Int } deriving (Show, Eq, Arbitrary)
+
+instance Semigroup Sub where
+  MkSub m <> MkSub n = MkSub (m - n)
+-}
 
 -- DEFINE various instances
 
@@ -178,17 +186,52 @@ test :: Semigroup m => (Int -> m) -> m
 test f = f 0 <> f 1 <> f 2
 
 
+-- DEFINE property tests
+
+prop_associative
+  :: (Eq m, Semigroup m)
+  => (Int -> m)
+  -> m -> m -> m -> Bool
+prop_associative f x y z = x <> (y <> z) == (x <> y) <> z
+
+-- DEFINE non-examples (e.g. minus)
 
 
 -- Monoids
 class Semigroup m => Monoid m where
   mempty :: m
 -- DISCUSS laws
+-- DEFINE property tests
 
+prop_neutral
+  :: (Eq m, Monoid m)
+  => (Int -> m)
+  -> m -> Bool
+prop_neutral f x =
+   x == mempty <> x
+   && x <> mempty == x
 
 -- EXTEND previous instances (if possible!)
 
+instance Monoid [a] where
+  mempty = []
 
+instance Monoid (Tsil a) where
+  mempty = MkTsil []
+
+{-
+instance Monoid (Empty a) where
+  mempty = MkEmpty []
+-}
+
+instance Monoid Sum where
+  mempty = MkSum 0
+
+instance Monoid Prod where
+  mempty = MkProd 1
+
+instance Monoid Max where
+  mempty = MkMax minBound -- sneaky!
 
 
 ------------------------------------------------------------------------
@@ -196,10 +239,52 @@ class Semigroup m => Monoid m where
 
 
 -- DEFINE foldList
+foldList :: Monoid m => [m] -> m
+foldList [] = mempty
+foldList (m:ms) = m <> foldList ms
+
+
+prop_foldList :: (Eq m, Monoid m)
+  => (Int -> m)
+  -> [m] -> [m] -> Bool
+prop_foldList f xs ys = foldList (xs <> ys) == foldList xs <> foldList ys
+
+
+newtype First a = MkFirst {getFirst :: Maybe a} deriving Show
+
+
 -- DEFINE First Monoid
+instance Semigroup (First a) where
+  (MkFirst Nothing) <> x = x
+  x <> _ = x
+
+instance Monoid (First a) where
+  mempty = MkFirst Nothing
+
+
+
+-- newtype
+
 -- EXAMPES
 
+
+
 -- DEFINE Foldable
+
+class Foldable t where
+  fold :: Monoid m => t m -> m
+
+data Formula a = Atom a | IsTrue | And (Formula a) (Formula a)
+
+
+instance Foldable Formula where
+   fold (Atom x) = x
+   fold IsTrue   = mempty
+   fold (And e f) = fold e <> fold f
+
+
+allAtoms :: Formula a -> [a]
+allAtoms = fold . fmap (\ a -> [a])
 -- DEFINE various instances
 
 
