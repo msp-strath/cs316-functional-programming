32ca9cf694aa3661e4247b585ef2aa7b531cef85
Guillaume Allais <guillaume.allais@ens-lyon.org>
2025-11-14 15:00:14 +0000

    [ backup ] today's code

--- lecture-notes/Week08Live.hs
+++ lecture-notes/Week08Live.hs
@@ -127,11 +127,39 @@ readLine = go []
 
 
 
-
-
 {- Part 8.4 : PARSER COMBINATORS -}
 
 -- If we can get input, how do we take it apart?
+type Parser_V1 a = String -> Maybe a
+
+boolean_V1 :: Parser_V1 Bool
+boolean_V1 "True" = Just True
+boolean_V1 "False" = Just False
+boolean_V1 _ = Nothing -- " True"
+
+
+newtype Parser a = MkParser {runParser :: String -> Maybe (a,String)}
+ deriving Functor
+
+instance Applicative Parser where
+  pure x = MkParser  (\str -> Just (x,str))
+  (<*>)  = ap
+
+instance Monad Parser where
+  mx >>= mf = MkParser (\str0 ->
+                           case runParser mx str0 of
+                               Nothing -> Nothing
+                               Just (x,str1) -> runParser (mf x) str1)
+orElseMaybe :: Maybe a -> Maybe a -> Maybe a
+orElseMaybe (Just x) _ = Just x
+orElseMaybe Nothing x  = x
+
+orElse :: Parser a -> Parser a -> Parser a
+orElse p1 p2 = MkParser (\str -> runParser p1 str `orElseMaybe` runParser p2 str)
+
+disj :: Parser l -> Parser r -> Parser (Either l r)
+disj l r = fmap Left l `orElse` fmap Right r
+
 
 -- type Parser_v1 a = String -> Maybe a
 
@@ -139,7 +167,115 @@ readLine = go []
 
 -- boolean_v1 :: Parser_v1 Bool
 
+failure :: Parser a
+failure = MkParser (\_ -> Nothing)
+
+char :: Parser Char
+char = MkParser go
+   where go (x:xs) = Just (x,xs)
+         go []     = Nothing
+
+expect :: (Char -> Bool) -> Parser Char
+expect p = do
+  c <- char
+  if p c then return c else failure
+
 -- How to parse pairs of Booleans?
+expectChar :: Char -> Parser ()
+expectChar c = () <$ expect (c ==)
+
+
+-- for [1..10] $ \ i -> (...)
+
+string :: String -> Parser ()
+string str = for_ str (\c -> expectChar c)
+
+boolean :: Parser Bool
+boolean = do string "True"
+             return True
+          `orElse`
+          do string "False"
+             return False
+
+
+many :: Parser a -> Parser [a]
+many pa = some pa `orElse` return []
+
+some :: Parser a -> Parser [a]
+some pa = do
+  a <- pa
+  as <- many pa
+  return (a : as)
+
+whitespace :: Parser ()
+whitespace = () <$ many (expect (`elem` " \t\r\n"))
+
+token :: String -> Parser ()
+token tk = do
+  whitespace
+  string tk
+  whitespace
+
+pbetween
+  :: Parser ()
+  -> Parser ()
+  -> Parser a
+  -> Parser a
+pbetween pstart pend pval = do
+  pstart
+  val <- pval
+  pend
+  return val
+
+pparens :: Parser a -> Parser a
+pparens = pbetween (token "(") (token ")")
+
+ppair :: Parser a -> Parser b -> Parser (a, b)
+ppair pa pb = pparens $ do
+  l <- pa
+  token ","
+  r <- pb
+  return (l,r)
+
+pSepBy :: Parser () -> Parser a -> Parser [a]
+pSepBy psep pval =
+  do val <- pval
+     vals <- many (do psep; pval)
+     return (val : vals)
+  `orElse` return []
+
+plist :: Parser a -> Parser [a]
+plist
+  = pbetween (token "[") (token "]")
+  . pSepBy (token ",")
+
+boolean2 :: Parser (Bool,Bool)
+boolean2 = ppair boolean boolean
+
+parse :: Parser a -> String -> Maybe a
+parse p str = case runParser p str of
+  Nothing -> Nothing
+  Just (x, []) -> Just x
+  Just (x, _) -> Nothing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- Problem: these parsers are "monolithic". There is no way to access
 -- the trailing input they couldn't parse.
