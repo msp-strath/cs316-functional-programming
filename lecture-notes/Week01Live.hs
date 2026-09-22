00143cbbddffed50a19d928b79bd5fd3f54acc37
Guillaume Allais <guillaume.allais@ens-lyon.org>
2026-09-22 11:02:46 +0100

    [ ship ] Lec1 skeleton

---
+++ lecture-notes/Week01Live.hs
@@ -0,0 +1,226 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+{-# LANGUAGE LambdaCase #-}
+module Week01Live where
+
+{-    WELCOME TO
+
+        CS316 λ>=
+
+          FUNCTIONAL PROGRAMMING
+
+
+      with
+        Alasdair Lambert
+        Guillaume Allais
+
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{- In this course, you will:
+
+     - Learn more about Functional Programming (in Haskell)
+
+
+
+   (Typed) Functional Programming is
+
+     - Defining Datatypes To Represent Problems
+
+     - Defining Functions To Create New Data From Old
+
+   a.k.a "Value-oriented" programming.
+
+   A "Functional Programming Language" is a programming language that
+   is designed to make it easy to use Functional Programming ideas. -}
+
+
+
+
+
+
+
+
+
+
+
+
+
+{- We use Haskell as an example Functional Programming Language.
+
+     - Many languages now include ideas originally from Functional Programming.
+
+        - Functions as values (a.k.a "lambdas")
+
+        - "Algebraic" data types; "Make Illegal States Unrepresentable"
+
+        - Immutability
+
+        - Expressive Types
+
+        - Errors as data, instead of Exceptions
+
+        - No 'null' (the "Billion dollar mistake")
+
+        - Close tracking of possible "side effects"
+
+   Haskell is not perfect (I will grumble about it during the course
+   [*]), but it does offer a place to learn about Functional
+   Programming concepts without too many distractions.
+
+   [*] "There are only two kinds of languages: the ones people
+       complain about and the ones nobody uses.”  ― Bjarne Stroustrup,
+       The C++ Programming Language
+-}
+
+
+
+
+
+
+
+{- Course arrangements:
+
+   - Lectures:
+     - Tuesdays at 11am
+     - Fridays  at 2pm
+
+   - Labs from Week 3 in Level 12 of Livingstone Tower
+     - Tuesdays at 13:00-15:00 :
+
+   - Holes:
+     - No lecture on Tuesday 29th September
+
+   - Video lectures, to support the in-person lectures
+     - ~ 6 videos / week
+     - ~ 10 minutes long
+
+   - Online lecture notes in a GitHub repository
+     - git clone https://github.com/msp-strath/cs316-functional-programming
+     - git pull
+
+     Feel free to send PRs if you spot mistakes!
+
+-}
+
+
+{- This is a programming course
+
+   You will be expected to do a lot of programming in order to understand
+   the concepts.
+
+   20 credit course : 12 hrs/week, 1 hour of videos, 2 of lectures, 2 labs.
+-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{- YOU WILL NEED A WORKING HASKELL INSTALLATION
+
+   - Suggested setup:
+
+       - GHCup (GHC, Cabal, HLS) + VSCode + Haskell extension.
+
+       - We use Emacs in the lectures and so does Bob in the videos
+
+   - There are instructions on MyPlace
+
+   - We (unfortunately) cannot test on Windows, so we will need the
+     class's help to iron out Windows problems.
+
+-}
+
+
+
+
+
+
+
+
+
+{- Assessment:
+
+   - One class test (50%) in the labs mediated via myplace
+        First attempt:    Week 6 (October 27th)
+
+   - One large coursework "mini-project" (50%)
+        Specification released Week 3
+        Submission Week 11 (December 11th)
+
+
+ Set-ExecutionPolicy Bypass -Scope Process -Force;[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; try { & ([ScriptBlock]::Create((Invoke-WebRequest https://www.haskell.org/ghcup/sh/bootstrap-haskell.ps1 -UseBasicParsing))) -Interactive -DisableCurl } catch { Write-Error $_ }
+
+-}
+
+
+-- Playing cards
+
+------------------------------------------------------------------------------
+-- Suits, and their colours
+
+-- exampleSuit :: Suit
+
+
+-- getColour :: Suit -> Colour
+
+
+------------------------------------------------------------------------------
+-- Modeling ranks
+
+-- numericValue :: Rank -> Int
+
+-- lessThanOrEqualValue :: Rank -> Rank -> Bool
+
+
+------------------------------------------------------------------------------
+-- A card is a suit together with a rank
+
+
+-- suitOfCard :: Card -> Suit
+
+
+------------------------------------------------------------------------------
+-- Generating a deck
+
+-- allSuits :: [Suit]
+
+-- allRanks :: [Rank]
+
+-- Using a list comprehension
+-- allCards :: [Card]
+
+-- Using applicative notations
+-- desk :: [Card]
